b901f24c6211f555748e2964c406f5f3339ca1c8
goudete <goudetenrique@gmail.com>
2022-02-11 17:17:58 -0600

    adding ability to connect Metamask

--- src/App.js
+++ src/App.js
@@ -1,13 +1,79 @@
+import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+
 import './App.scss';
 
-function App() {
+const App = () => {
+    
+    const [currentAccount, setCurrentAccount] = useState("");
+    
+    const checkIfWalletIsConnected = async () => {
+        const { ethereum } = window;
+
+        if (!ethereum) {
+            console.log('Make sure you have MetaMask!');
+            return;
+        }
+
+        const chainId = await ethereum.request({ method: 'eth_chainId' });
+        console.log("Connected to chain " + chainId);
+        const rinkebyChainId = "0x4";
+
+        if (chainId !== rinkebyChainId) {
+            console.log("You are not connected to the Rinkeby Test Network!");
+            return;
+        }
+
+        console.log("We have the ethereum object", ethereum);
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+        if (accounts.length !== 0) {
+            const account = accounts[0];
+            console.log('found authorized account:', account);
+            setCurrentAccount(account);
+
+            // setupEventListener();
+        } else {
+            console.log("No authorized account found")
+        }
+    }
+
+    const connectWallet = async () => {
+        try {
+            const { ethereum } = window;
+
+            if (!ethereum) {
+                alert('Get MetaMask!');
+                return;
+            }
+            const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+            console.log('Connected:', accounts[0]);
+            setCurrentAccount(accounts[0]);
+            
+            // setupEventListener();
+            return alert('Account connected!');
+        } catch(err) {
+            console.log(err);
+
+        }
+    };
+
+    useEffect(() => {
+        checkIfWalletIsConnected();
+    }, []);
+
     return (
         <div className='container'>
             <div className='container__header'>
             </div>
             <div className='container__body'>
-                <div className='container__body-mint'>
-                    
+                <div>
+                    <button onClick={connectWallet}>connect wallet</button> <br />
+                    {
+                        currentAccount.length > 0 
+                        ? `Connected Account: ${currentAccount}`
+                        : 'No connected account'
+                    }
                 </div>
             </div>
             <div className='container__footer'>
